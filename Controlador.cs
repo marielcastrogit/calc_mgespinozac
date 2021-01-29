1c0653c5add9d913b70404c97b53e1fcc3fc4639
marielcastro <marielcastrogit@gmail.com>
2021-01-29 05:23:46 -0600

    Validacion del textBox para que solo admita numeros

--- Controlador.cs
+++ Controlador.cs
@@ -21,9 +21,27 @@ namespace CalculadoraWPF
             /*A traves del metodo source en el objeto e obtenemos el origen del evento
              y lo comparamos con el control de textBox perteneciente al objeto window
             de la clase MainWindow*/
-            if(e.Source == window.txtBox)
+            if (e.Source == window.txtBox)
             {
-                MessageBox.Show("Objeto enviado: " + sender.ToString() + "Evento" + e.ToString());
+                /*Si el texto es una letra e.Handled = true es decir
+                 que no lo deje continuar
+                e.Handler = true -> no continuar
+                e.Handler = false -> continuar
+                */
+                string caracteres = e.Text;
+                char []caracter = caracteres.ToCharArray();
+
+                /*Segun el codigo ASCII para que sea una letra tiene que estar 
+                 entre 32 y 64 o entre 58 y 255*/
+                
+                if ((caracter[0] >= 32 && caracter[0] <= 47) || (caracter[0]>=58 && caracter[0]<=255))
+                { 
+                    e.Handled = true; 
+                }
+                else
+                {
+                    e.Handled = false;
+                }
             }
         }
     }
